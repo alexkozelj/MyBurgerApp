b80c3b2da221eb2ac7a5084392770f44b9a4e01e
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2020-04-26 11:56:37 +0200

    changing folder structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Auxiliary/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -101,10 +101,10 @@ class BurgerBuilder extends Component {
                show={this.state.purchasing}
                modalClosed={this.purchaseCancelHandler}>
                   <OrderSummary 
-                  ingredients={this.state.ingredients}
-                  price={this.state.totalPrice}
-                  purchaseCancelled={this.purchaseCancelHandler}
-                  purchaseContinued={this.purchaseContinueHandler}
+                     ingredients={this.state.ingredients}
+                     price={this.state.totalPrice}
+                     purchaseCancelled={this.purchaseCancelHandler}
+                     purchaseContinued={this.purchaseContinueHandler}
                   />  
             </Modal>
             <Burger ingredients={this.state.ingredients} />
